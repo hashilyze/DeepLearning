57d38eb697289e8b7fcf0d5366240b36577e7ad7
hashilyze <hashilyze@gmail.com>
2024-04-04 12:23:13 +0900

    Feat: AutoEncoder train/test

--- AutoEncoder/main_test_AE.py
+++ AutoEncoder/main_test_AE.py
@@ -5,7 +5,50 @@ import numpy as np
 
 if __name__ == "__main__":
     print("Hi. I am an AutoEncoder Tester.")
-    ### Type your code here -- begin
 
+    data_loader = MNISTData()
+    data_loader.load_data()
+    ### Type your code here -- begin
+    auto_encoder = AutoEncoder()
+    auto_encoder.build_model()
+    # 로드
+    load_path = "./model/ae_model.weights.h5"
+    auto_encoder.load_weights(load_path)
+    print("load model weights from %s" % load_path)
+    
+    # 모델 평가 
+    num_test_items = 56
+    # 정답 이미지
+    test_data = data_loader.x_test[: num_test_items, :]
+    test_label = data_loader.y_test[: num_test_items]
+    # Flatten 이미지를 2차원으로 복원
+    test_data_x_print = test_data.reshape(num_test_items, data_loader.width, data_loader.height)
+
+    print("const by codes")
+    # 복원(예측) 이미지
+    reconst_data = auto_encoder.en_decoder.predict(test_data)
+    reconst_data_x_print = reconst_data.reshape(num_test_items, data_loader.width, data_loader.height)
+    reconst_data_x_print = tf.math.sigmoid(reconst_data_x_print)
+    # 정답과 복원 쌍 비교
+    MNISTData.print_56_pair_images(test_data_x_print, reconst_data_x_print, test_label)
+
+    print("const by code means for each digit")
+    # 각 숫자마다 code의 평균을 계산해 출력: 공통 패턴 관찰
+    avg_codes = np.zeros([10, 32])
+    avg_add_cnt = np.zeros([10])
+    
+    latent_vecs = auto_encoder.encoder.predict(test_data)
+    for i, label, in enumerate(test_label):
+        avg_codes[label] = latent_vecs[i]
+        avg_add_cnt[label] += 1.0
+    for i in range(10):
+        if avg_add_cnt[label] > 0.1:
+            avg_codes[i] /= avg_add_cnt[label]
+
+    avg_code_tensor = tf.convert_to_tensor(avg_codes)
+    reconst_data_by_vecs = auto_encoder.decoder.predict(avg_code_tensor)
+    reconst_data_x_by_mean_print = reconst_data_by_vecs.reshape(10, data_loader.width, data_loader.height)
+    label_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    MNISTData.print_10_images(reconst_data_x_by_mean_print, label_list)
     ### Type your code here -- end
 
--- AutoEncoder/main_train_AE.py
+++ AutoEncoder/main_train_AE.py
@@ -11,6 +11,16 @@ if __name__ == "__main__":
     data_loader.load_data()
 
     ### Type your code here -- begin
+    x_train = data_loader.x_train   # En-Decoder의 입력이자 정답
+    input_output_dim = data_loader.in_out_dim
 
+    auto_encoder = AutoEncoder()
+    auto_encoder.build_model()
+    auto_encoder.fit(x=x_train, y=x_train, batch_size=batch_size, epochs=num_epochs)
+    
+    # 저장
+    save_path = "./model/ae_model.weights.h5"
+    auto_encoder.save_weights(save_path)
+    print("save model weights to %s" % save_path)
     ### Type your code here -- end
 
